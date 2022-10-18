d6f0287341449f234c9b00d4521336dfb228873c
Tiffany Darling <TheCanterlonian@users.noreply.github.com>
2022-10-17 17:23:17 -0700

    Update dynipmon.cs
    created file insurance function

--- dynipmon.cs
+++ dynipmon.cs
@@ -10,4 +10,22 @@ using System.Data;
 //dynipmon by Tiffany Darling
 namespace dynipmon
 {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//check the log file exists and if not, create it
+			//
+		}
+		//checks if a file exists and if it doesn't, creates the file so it does exist
+		static bool fileInsurance(string fileToEnsure) //returns 1 if file exists, 0 if file created
+		{
+			if (!(File.Exists(fileToEnsure)))
+			{
+				File.Create(fileToEnsure);
+				return 0;
+			}
+			return 1;
+		}
+	}
 }
