c58d09dca4bb7bcb98d99823e2a2045fa480f5a1
Tiffany Darling <TheCanterlonian@users.noreply.github.com>
2022-10-19 09:31:10 -0700

    Update dynipmon.cs

--- dynipmon.cs
+++ dynipmon.cs
@@ -14,28 +14,44 @@ namespace dynipmon
 	{
 		static void Main(string[] args)
 		{
-			fileInsurance("./dim.log"); //Ensure the log file
-			bool configExisted = fileInsurance("./dynipmon.cfg"); //Ensure the config file
-			//initialize function-wide variables
-			string logFile = "./dim.log"
-			string currentFile = "./dynipmon.cfg"
-			//initialize program state
-			if (!(configExisted)) //see if it didn't exist and if not then try to initialize the file
+			if (!(args.Length == 0)) //checks if any arguments were used
 			{
-				overwriteFile(currentFile, "./public-ip.txt");
-				//no matter what happens, don't exit the block without doing this
-				currentFile = File.ReadAllText(currentFile);
+				//makes all arguments lowercase
+				for (int i = 0; i < args.Length; i++)
+				{
+					args[i] = args[i].ToLower();
+				}
+				//checks if "skip logging" option was passed
+				if (args.Contains("-s"))
+				{
+					//start the async task for checking the IP address and sending it to standard output
+				}
 			}
-			else //otherwise just load from the file
+			else
 			{
-				//no matter what happens, don't exit the block without doing this
-				currentFile = File.ReadAllText(currentFile);
+				fileInsurance("./dim.log"); //Ensure the log file
+				bool configExisted = fileInsurance("./dynipmon.cfg"); //Ensure the config file
+				//initialize function-wide variables
+				string logFile = "./dim.log"
+				string currentFile = "./dynipmon.cfg"
+				//initialize program state
+				if (!(configExisted)) //see if it didn't exist and if not then try to initialize the file
+				{
+					overwriteFile(currentFile, "./public-ip.txt");
+					//no matter what happens, don't exit the block without doing this
+					currentFile = File.ReadAllText(currentFile);
+				}
+				else //otherwise just load from the file
+				{
+					//no matter what happens, don't exit the block without doing this
+					currentFile = File.ReadAllText(currentFile);
+				}
+				appendFile(logFile, "Dynipmon program start.\n");
+				//grab the last recorded IP address in the current working file
+				string recordedIP = File.ReadAllText(currentFile);
+				appendFile(logFile, "Initially recorded IP was: " + recordedIP + "\n");
+				//start the two async tasks for checking & recording the IP address and waiting for input & interupting the program tasks
 			}
-			appendFile(logFile, "Dynipmon program start.\n");
-			//grab the last recorded IP address in the current working file
-			string recordedIP = File.ReadAllText(currentFile);
-			appendFile(logFile, "Initially recorded IP was: " + recordedIP + "\n");
-			//start the two async tasks for checking & recording the IP address and waiting for input & interupting the program tasks
 		}
 		//checks if a file exists and if it doesn't, creates the file so it does exist
 		static bool fileInsurance(string fileToEnsure) //returns True if file exists, False if file created
