ebfab2877446bc52079e000a230a045df811e380
Tiffany Darling <TheCanterlonian@users.noreply.github.com>
2022-10-19 09:07:45 -0700

    Update dynipmon.cs
    stuff

--- dynipmon.cs
+++ dynipmon.cs
@@ -15,35 +15,27 @@ namespace dynipmon
 		static void Main(string[] args)
 		{
 			fileInsurance("./dim.log"); //Ensure the log file
-			bool configExisted=fileInsurance("./dynipmon.cfg"); //Ensure the config file
+			bool configExisted = fileInsurance("./dynipmon.cfg"); //Ensure the config file
 			//initialize function-wide variables
-			string currentFile = "./dim.log"
+			string logFile = "./dim.log"
+			string currentFile = "./dynipmon.cfg"
+			//initialize program state
 			if (!(configExisted)) //see if it didn't exist and if not then try to initialize the file
 			{
-				try
-				{
-					WriteAllText("./dynipmon.cfg", "./public-ip.txt");
-				}
-				catch (IOException e)
-				{
-					Console.WriteLine("General input/output error.");
-				}
-				catch (UnauthorizedAccessException e)
-				{
-					Console.WriteLine("General permissions error.");
-				}
-				catch (SecurityException e)
-				{
-					Console.WriteLine("Specific permissions error.");
-				}
+				overwriteFile(currentFile, "./public-ip.txt");
 				//no matter what happens, don't exit the block without doing this
-				currentFile = ReadAllText("./dynipmon.cfg");
+				currentFile = File.ReadAllText(currentFile);
 			}
 			else //otherwise just load from the file
 			{
 				//no matter what happens, don't exit the block without doing this
-				currentFile = ReadAllText("./dynipmon.cfg");
+				currentFile = File.ReadAllText(currentFile);
 			}
+			appendFile(logFile, "Dynipmon program start.\n");
+			//grab the last recorded IP address in the current working file
+			string recordedIP = File.ReadAllText(currentFile);
+			appendFile(logFile, "Initially recorded IP was: " + recordedIP + "\n");
+			//start the two async tasks for checking & recording the IP address and waiting for input & interupting the program tasks
 		}
 		//checks if a file exists and if it doesn't, creates the file so it does exist
 		static bool fileInsurance(string fileToEnsure) //returns True if file exists, False if file created
@@ -54,6 +46,16 @@ namespace dynipmon
 				return False;
 			}
 			return True;
+		}
+		//overwrites a file in entirety, also handles errors associated with file overwrites
+		static bool overwriteFile(string file, string text) //returns True if overwrite succeeds, False if it fails
+		{
+			//
+		}
+		//appends a file with new content to be added, also handles errors associated with concatenation
+		static bool appendFile(string file, string content) //returns True if overwrite succeeds, False if it fails
+		{
+			//
 		}
 	}
 }
